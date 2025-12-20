1489530043623eab421b4e9078ff0bcec9817b45
jsmellie <jeremysmellie@gmail.com>
2025-12-20 10:50:29 -0500

    Changed up logger to be more robust and to place the log file in a better location and have a better naming convention

--- src/eom-insight.py
+++ src/eom-insight.py
@@ -72,22 +72,55 @@ def importCSV(filePath):
         # - Update Google sheet with information
         # - Add support for more institutions
         # ---
-   
 
+def run_once(f):
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
+
+@run_once
 def setupRootLogger():
+    import datetime
+    import os
+    
+    logFolderName = 'logs'
+    
+    # Make sure that the log folder exists
+    curDir = os.path.dirname(os.path.realpath(__file__))
+    logFolder = os.path.join(curDir, '..', logFolderName)
+    if not os.path.exists(logFolder):
+        os.makedirs(logFolder)
+        
+    # Determine log file name
+    formattedTime = datetime.datetime.now().strftime('%Y-%m-%d')
+    fileNameFormat = f'general_{formattedTime}.log'
+    localFilePath = os.path.join(logFolderName, fileNameFormat)
+        
+    # Create logger
     root = logging.getLogger()
     root.setLevel(logging.INFO)
-    # Create console handler with a higher log level
+    
+    # Add a blank line in the log file to separate runs
+    osFilePath = os.path.join(logFolder, fileNameFormat)
+    if (os.path.exists(osFilePath) and os.path.getsize(osFilePath) > 0):
+        bh = logging.FileHandler(localFilePath)
+        bh.setLevel(logging.INFO)
+        bh.setFormatter(logging.Formatter(''))
+        root.addHandler(bh)
+        root.info('')
+        root.info('')
+        root.removeHandler(bh)
+    
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    
-    fh = logging.FileHandler('app.log')
+    fh = logging.FileHandler(localFilePath)
     fh.setLevel(logging.INFO)
-    # Create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     fh.setFormatter(formatter)
-    # Add the handlers to the logger
     root.addHandler(ch)
     root.addHandler(fh)
     pass   
